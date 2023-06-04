e3a2c47dcdea71691bdae2b818ceaaf8e9dcef44
Nikolas <nkls99@gmail.com>
2023-06-04 17:15:23 -0300

    BUG-FIX: cannot delete because of fk restrains

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -19,19 +19,19 @@ async function CreateTables() {
 
   await client.query(`CREATE TABLE batteries (
     id SERIAL PRIMARY KEY,
-    surfer_one INTEGER NOT NULL REFERENCES surfers(number),
-    surfer_two INTEGER NOT NULL REFERENCES surfers(number)
+    surfer_one INTEGER NOT NULL REFERENCES surfers(number) ON DELETE CASCADE,
+    surfer_two INTEGER NOT NULL REFERENCES surfers(number) ON DELETE CASCADE
   )`, [])
 
   await client.query(`CREATE TABLE waves (
     id SERIAL PRIMARY KEY,
-    battery INTEGER NOT NULL REFERENCES batteries(id),
-    surfer INTEGER NOT NULL REFERENCES surfers(number)
+    battery INTEGER NOT NULL REFERENCES batteries(id) ON DELETE CASCADE,
+    surfer INTEGER NOT NULL REFERENCES surfers(number) ON DELETE CASCADE
   )`, [])
 
   await client.query(`CREATE TABLE grades (
     id SERIAL PRIMARY KEY,
-    wave INTEGER NOT NULL REFERENCES waves(id),
+    wave INTEGER NOT NULL REFERENCES waves(id) ON DELETE CASCADE,
     parcial_one INTEGER NOT NULL,
     parcial_two INTEGER NOT NULL,
     parcial_three INTEGER NOT NULL
